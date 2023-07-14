af69181f91606e6b03c29d0d8d27fd4a421bd644
javierb256 <44247277+javierb256@users.noreply.github.com>
2023-07-13 18:13:30 -0700

    fixed styling on bookingForm. Added page change when submitting the form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import * as Yup from "yup";
 import { useFormik } from "formik";
 import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
 
 function BookingForm(props) {
   const times = props.time;
-  const [occasion, setOccasion] = useState("");
-
+  const navigate = useNavigate();
   const minDate = dayjs().format("MM/DD/YYYY");
   const formik = useFormik({
     initialValues: {
@@ -18,17 +17,17 @@ function BookingForm(props) {
     validationSchema: Yup.object({
       guests: Yup.number()
         .integer()
-        .min(1, "minimum guests of 1")
+        .min(1, "Minimum guests of 1")
         .required("Must enter number of guests"),
       date: Yup.date()
-        .min(minDate, "date must be today or later")
+        .min(minDate, "Date must be today or later")
         .required("Required"),
       time: Yup.string()
         .required("Please select a time")
         .label("Choose a time"),
     }),
     onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+      navigate("/contact-information");
     },
   });
 
@@ -36,49 +35,47 @@ function BookingForm(props) {
     <>
       <h2>Reserve a Table</h2>
       <form className="bookingForm" onSubmit={formik.handleSubmit}>
-        <label htmlFor="date">Choose a date</label>
-        <input
-          type="date"
-          id="date"
-          //returns the selected date inside the dispatch parameter for the availableTimes useReducer
-          onClick={e => props.dispatch({ type: e.target.value })}
-          {...formik.getFieldProps("date")}
-        />
-        {formik.touched.date && formik.errors.date ? (
-          <div>{formik.errors.date}</div>
-        ) : null}
+        <div className="detail">
+          <label htmlFor="date">Choose a date</label>
+          <input
+            type="date"
+            id="date"
+            //returns the selected date inside the dispatch parameter for the availableTimes useReducer
+            onClick={e => props.dispatch({ type: e.target.value })}
+            {...formik.getFieldProps("date")}
+          />
+          {formik.touched.date && formik.errors.date ? (
+            <div>{formik.errors.date}</div>
+          ) : null}
+        </div>
 
-        <label htmlFor="time">Choose time</label>
-        <select id="time" {...formik.getFieldProps("time")}>
-          {/* populates select element with options from availableTimes state */}
-          {times?.map((element, index) => {
-            return <option key={index}>{element}</option>;
-          })}
-        </select>
+        <div className="detail">
+          <label htmlFor="time">Choose time</label>
+          <select id="time" {...formik.getFieldProps("time")}>
+            {/* populates select element with options from availableTimes state */}
+            {times?.map((element, index) => {
+              return <option key={index}>{element}</option>;
+            })}
+          </select>
 
-        {formik.touched.time && formik.errors.time ? (
-          <div>{formik.errors.time}</div>
-        ) : null}
-        <label htmlFor="guests">Number of guests</label>
-        <input
-          type="number"
-          min="1"
-          id="guests"
-          {...formik.getFieldProps("guests")}
-        />
-        {formik.touched.guests && formik.errors.guests ? (
-          <div>{formik.errors.guests}</div>
-        ) : null}
-        <label htmlFor="occasion">Occasion</label>
-        <select
-          id="occasion"
-          value={occasion}
-          onChange={e => setOccasion(e.target.value)}
-        >
-          <option></option>
-          <option>Birthday</option>
-          <option>Anniversary</option>
-        </select>
+          {formik.touched.time && formik.errors.time ? (
+            <div>{formik.errors.time}</div>
+          ) : null}
+        </div>
+
+        <div className="detail">
+          <label htmlFor="guests">Number of guests</label>
+          <input
+            type="number"
+            min="1"
+            id="guests"
+            {...formik.getFieldProps("guests")}
+          />
+          {formik.touched.guests && formik.errors.guests ? (
+            <div>{formik.errors.guests}</div>
+          ) : null}
+        </div>
+
         <button
           className="custom-button"
           type="submit"
