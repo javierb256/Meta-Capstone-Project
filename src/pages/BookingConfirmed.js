a3cae69caadaf0ae66c39d63fbe4f9e1ff13fc2c
javierb256 <44247277+javierb256@users.noreply.github.com>
2023-07-20 19:30:52 -0700

    add js confetti library.Added confetti to confirmation page

--- src/pages/BookingConfirmed.js
+++ src/pages/BookingConfirmed.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
+import JSConfetti from "js-confetti";
 
 function BookingConfirmed() {
-    return (
-        <div className="confirmation-container container">
-            <h3>Table Reserved</h3>
-            <p>Your table has been reserved successfully! You will receive an email confirmation with your reservation details shortly. We are excited to see you soon!</p>
-            <Link to={'/booking'} className="custom-button final-confirmation">Book Another Table</Link>
-        </div>
-    )
+  const jsConfetti = new JSConfetti();
+  jsConfetti.addConfetti();
+
+  return (
+    <div className="confirmation-container container">
+      <h3>Table Reserved</h3>
+      <p>
+        Your table has been reserved successfully! You will receive an email
+        confirmation with your reservation details shortly. We are excited to
+        see you soon!
+      </p>
+      <Link to={"/booking"} className="custom-button final-confirmation">
+        Book Another Table
+      </Link>
+    </div>
+  );
 }
 
 export default BookingConfirmed;
